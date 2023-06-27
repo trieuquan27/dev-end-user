823c7b398c7f0c1ae47af6ff94bfd2ecbf9dd9a2
trieu_quan <trieu_quan@fastboy.net>
2023-06-27 17:15:04 +0700

    fourth commit

--- e2e/Desktop/AddFunds/AddFundsSuccess.spec.ts
+++ e2e/Desktop/AddFunds/AddFundsSuccess.spec.ts
@@ -1,20 +1,18 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 // test.use({viewport:{width:490,height:896}}),
-test('Add Funds Success', async ({ page }) => {
-  await page.goto('/');
+test("Add Funds Success", async ({ page }) => {
+  await page.goto("/");
   await page.click("(//button[contains(@class,'flex items-center')])[1]");
-  await expect(page).toHaveURL('/signin');
-  await page.fill('input[name="username"]', 'a@a68.com');
-  await page.fill('input[name="password"]', 'Trieu123456789@');
+  await expect(page).toHaveURL("/signin");
+  await page.fill('input[name="username"]', "a@a68.com");
+  await page.fill('input[name="password"]', "Trieu123456789@");
   await page.click("(//button[contains(@class,'flex items-center')])[1]");
-  await expect(page).toHaveURL('/');
+  await expect(page).toHaveURL("/");
   await page.click("((//button[contains(@class,'flex items-center')])[1])");
   await page.click("(//img[@class='mr-2'])[1]");
   await page.click("(//div[contains(@class,'w-full flex-none')]//button[1])");
-  await page.fill("input","1000");
+  await page.fill("input", "1000");
   await page.click("//div[@class='flex items-center']");
   await page.waitForTimeout(3000);
-  await expect(page.getByText('Add funds successfully!')).toBeVisible();
-
-
+  await expect(page.getByText("Add funds successfully!")).toBeVisible();
 });
