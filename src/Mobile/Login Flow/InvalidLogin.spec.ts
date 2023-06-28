457a6adc3f9fee05f5dd93f70cd7fe0ba1f0e815
trieu_quan <trieu_quan@fastboy.net>
2023-06-28 11:07:22 +0700

    update button mobile UI script

--- src/Mobile/Login Flow/InvalidLogin.spec.ts
+++ src/Mobile/Login Flow/InvalidLogin.spec.ts
@@ -1,13 +1,13 @@
-import { expect, test } from '@playwright/test';
-test.use({viewport:{width:490,height:896}}),
-test('Login account', async ({ page }) => {
-  await page.goto('/');
-  await page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]");
-  await page.click("(//button[contains(@class,'flex items-center')])[3]");
-  await expect(page).toHaveURL('/signin');
-  await page.fill('input[name="username"]', 'a@a18.com');
-  await page.fill('input[name="password"]', 'Trieu123456789');
-  await page.click("(//button[contains(@class,'flex items-center')])[1]");
-  await page.waitForTimeout(3000);
-  await expect(page.getByText('Invalid')).toBeVisible();
-});
+import { expect, test } from "@playwright/test";
+test.use({ viewport: { width: 490, height: 896 } }),
+  test("Login account", async ({ page }) => {
+    await page.goto("/");
+    await page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]");
+    await page.click("(//span[contains(@class,'flex items-center')])[3]");
+    await expect(page).toHaveURL("/signin");
+    await page.fill('input[name="username"]', "a@a18.com");
+    await page.fill('input[name="password"]', "Trieu123456789");
+    await page.click("(//button[contains(@class,'flex items-center')])[1]");
+    await page.waitForTimeout(3000);
+    await expect(page.getByText("Invalid")).toBeVisible();
+  });
--- src/Mobile/Login Flow/ValidLogin.spec.ts
+++ src/Mobile/Login Flow/ValidLogin.spec.ts
@@ -1,12 +1,12 @@
-import { expect, test } from '@playwright/test';
-test.use({viewport:{width:490,height:896}}),
-test('Login account', async ({ page }) => {
-  await page.goto('/');
-  await page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]");
-  await page.click("(//button[contains(@class,'flex items-center')])[3]");
-  await expect(page).toHaveURL('/signin');
-  await page.fill('input[name="username"]', 'a@a68.com');
-  await page.fill('input[name="password"]', 'Trieu123456789@');
-  await page.click("(//button[contains(@class,'flex items-center')])[1]");
-  await expect(page).toHaveURL('/');
-});
+import { expect, test } from "@playwright/test";
+test.use({ viewport: { width: 490, height: 896 } }),
+  test("Login account", async ({ page }) => {
+    await page.goto("/");
+    await page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]");
+    await page.click("(//span[contains(@class,'flex items-center')])[3]");
+    await expect(page).toHaveURL("/signin");
+    await page.fill('input[name="username"]', "a@a68.com");
+    await page.fill('input[name="password"]', "Trieu123456789@");
+    await page.click("(//button[contains(@class,'flex items-center')])[1]");
+    await expect(page).toHaveURL("/");
+  });
