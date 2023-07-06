5bcfdd7ea6df47046aa8b2bd2af5abfc0fa6b2a7
trieu_quan <trieu_quan@fastboy.net>
2023-07-06 17:43:55 +0700

    combine mobile invalid login flow into 1 file

--- src/Mobile/LoginFlow/LogInEmailRequired.spec.ts
+++ src/Mobile/LoginFlow/LogInEmailRequired.spec.ts
@@ -1,12 +1,12 @@
-import { expect, test } from "@playwright/test";
-test.use({ viewport: { width: 490, height: 896 } }),
-  test("Login account", async ({ page }) => {
-    await page.goto("/");
-    await page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]");
-    await page.click("(//span[contains(@class,'flex items-center')])[3]");
-    await expect(page).toHaveURL("/signin");
-    await page.click("(//button[contains(@class,'flex items-center')])[1]");
-    await page.waitForTimeout(3000);
-    await expect(page.getByText("Email address is required")).toBeVisible();
-    page.close();
-  });
+// import { expect, test } from "@playwright/test";
+// test.use({ viewport: { width: 490, height: 896 } }),
+//   test("Login Email Required", async ({ page }) => {
+//     await page.goto("/");
+//     await page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]");
+//     await page.click("(//span[contains(@class,'flex items-center')])[3]");
+//     await expect(page).toHaveURL("/signin");
+//     await page.click("(//button[contains(@class,'flex items-center')])[1]");
+//     await page.waitForTimeout(3000);
+//     await expect(page.getByText("Email address is required")).toBeVisible();
+//     page.close();
+//   });
