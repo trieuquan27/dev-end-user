daa2718f3575c9151379d0a5881175bf3e11d144
trieu_quan <trieu_quan@fastboy.net>
2023-07-13 16:55:10 +0700

    add https credential to mailhog

--- src/Mailhog/mailhog.spec.ts
+++ src/Mailhog/mailhog.spec.ts
@@ -9,7 +9,16 @@ test("mailhog", async ({ browser }) => {
     },
   });
   const page = await context.newPage();
+  // Navigate to mailhog page
   await page.goto("https://deal.mailhog.fastboy.dev/");
+  await expect(page).toHaveURL("https://deal.mailhog.fastboy.dev/");
+  await page
+    .locator("(//div[contains(@class,'msglist-message row')])[1]")
+    .click();
+  const wait = page.waitForTimeout(2000);
+  await wait;
+  await page.getByTitle("Back to Inbox").click();
+  await wait;
   await expect(page).toHaveURL("https://deal.mailhog.fastboy.dev/");
   await page.close();
 });
