85b395a5bf368a8071b238a96cb89f0516b65157
trieu_quan <trieu_quan@fastboy.net>
2023-07-13 16:00:52 +0700

    add https credential to mailhog

--- src/Desktop/PlaceOrder(Logged)/RemoveCart(Logged).spec.ts
+++ src/Desktop/PlaceOrder(Logged)/RemoveCart(Logged).spec.ts
@@ -1,11 +1,6 @@
 import { expect, test } from "@playwright/test";
 import { userName } from "../../common/AccountList";
-test.afterEach(async ({ page }, testInfo) => {
-  console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
 
-  if (testInfo.status !== testInfo.expectedStatus)
-    console.log(`Did not run as expected, ended up at ${page.url()}`);
-});
 test("Remove Cart Logged Case", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
--- src/Mailhog/mailhog.spec.ts
+++ src/Mailhog/mailhog.spec.ts
@@ -1,11 +1,5 @@
 import { test, expect } from "@playwright/test";
-//test AftereEach
-// test.afterEach(async ({ page }, testInfo) => {
-//   console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
 
-//   if (testInfo.status !== testInfo.expectedStatus)
-//     console.log(`Did not run as expected, ended up at ${page.url()}`);
-// });
 // test script
 test("mailhog", async ({ browser }) => {
   const context = await browser.newContext({
