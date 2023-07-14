44b294a9d33969eb6ace0e25b029114545f2ed72
trieu_quan <trieu_quan@fastboy.net>
2023-07-14 09:26:16 +0700

    refacotr mobile  place order guest

--- src/Desktop/PlaceOrderGuest/GuestFail.spec.ts
+++ src/Desktop/PlaceOrderGuest/GuestFail.spec.ts
@@ -1,6 +1,13 @@
 import { expect, test } from "@playwright/test";
-
-test("Guest Fail", async ({ browser }) => {
+import { randomEmail } from "../../common/Randomemail";
+import {
+  randomAddress,
+  randomFirstName,
+  randomLastName,
+} from "../../common/randomname";
+import { credit } from "../../common/CreditCard";
+import { randomCVV, randomZipCode } from "../../common/RandomNumber";
+test("Guest Please add card to process payment!", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   //Navigate to mainpage
@@ -23,4 +30,61 @@ test("Guest Fail", async ({ browser }) => {
     page.getByText("Please add card to process payment!")
   ).toBeVisible();
   page.close();
+});
+
+test("Guest disagree terms", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  //Navigate to homepage
+  await page.goto("/");
+  await expect(page).toHaveURL("/");
+  //Buy Voucher
+  await page.click("(//img[@class='object-cover'])[1]");
+  await page.getByRole("button", { name: "Buy Now" }).dblclick();
+  await page.waitForTimeout(3000);
+  //Add cart item
+  await expect(page).toHaveURL("/cart");
+  await page.getByRole("button", { name: "Proceed To Checkout" }).click();
+  await expect(page).toHaveURL(/.*checkout/);
+  //Add email
+  await page.click("input[placeholder='Enter email']");
+  await page.fill("input[placeholder='Enter email']", `${randomEmail}`);
+  //Check term checkbox is unchecked
+  await page.getByRole("checkbox").nth(1).click();
+  await expect(page.getByRole("checkbox").nth(1)).toHaveAttribute(
+    "data-state",
+    "unchecked"
+  );
+  await expect(page.locator("//span[text()='Place Order']")).toBeDisabled();
+  await page.waitForTimeout(1000);
+  await page.close();
+});
+test("Guest invalid email", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveURL("/");
+  await page.click("(//img[@class='object-cover'])[1]");
+  await page.getByRole("button", { name: "Buy Now" }).click({ delay: 200 });
+  await expect(page).toHaveURL("/cart");
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Proceed To Checkout" }).click();
+  await expect(page).toHaveURL(/.*checkout/);
+  await page.waitForTimeout(6000);
+  await page.fill('//input[@placeholder="Enter email"]', "d@d+11.com");
+  await page.click("//button[text()='Add']");
+  await page.fill('input[name="firstName"]', `${randomFirstName}`);
+  await page.fill('input[name="lastName"]', `${randomLastName}`);
+  await page.fill('input[name="cardNumber"]', `${credit.mastercard}`);
+  await page.fill('input[name="expireDate"]', "12/26");
+  await page.fill('input[name="cvv"]', `${randomCVV}`);
+  await page.fill('input[name="address"]', `${randomAddress}`);
+  await page.fill('input[name="zipcode"]', `${randomZipCode}`);
+  await page.waitForTimeout(3000);
+  await page.click("//button[normalize-space()='Add Card']");
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Place Order" }).click();
+  await page.waitForTimeout(3000);
+  await expect(
+    page.getByText("This value is not a valid email address.")
+  ).toBeVisible();
+  await page.close();
 });
--- src/Mobile/PlaceOrder(Guest)/ByInvalidEmailPlaceorder.spec.ts
+++ src/Mobile/PlaceOrder(Guest)/ByInvalidEmailPlaceorder.spec.ts
@@ -1,14 +1,8 @@
 import { expect, test } from "@playwright/test";
-//test AfterEach
-test.afterEach(async ({ page }, testInfo) => {
-  console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
-
-  if (testInfo.status !== testInfo.expectedStatus)
-    console.log(`Did not run as expected, ended up at ${page.url()}`);
-});
+import { randomEmail } from "../../common/Randomemail";
 // Mobile viewport
 test.use({ viewport: { width: 490, height: 896 } });
-test("Invalid Mobile Place order by Guest", async ({ page }) => {
+test("Invalid email place order by Guest", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveURL("/");
   await page.click("(//img[@class='object-cover'])[1]");
@@ -35,4 +29,55 @@ test("Invalid Mobile Place order by Guest", async ({ page }) => {
   await expect(
     page.getByText("This value is not a valid email address.")
   ).toBeVisible();
+});
+test("Guest disagree terms", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  //Navigate to homepage
+  await page.goto("/");
+  await expect(page).toHaveURL("/");
+  //Buy Voucher
+  await page.click("(//img[@class='object-cover'])[1]");
+  await page.getByRole("button", { name: "Buy Now" }).dblclick();
+  await page.waitForTimeout(3000);
+  //Add cart item
+  await expect(page).toHaveURL("/cart");
+  await page.getByRole("button", { name: "Proceed To Checkout" }).click();
+  await expect(page).toHaveURL(/.*checkout/);
+  //Add email
+  await page.click("input[placeholder='Enter email']");
+  await page.fill("input[placeholder='Enter email']", `${randomEmail}`);
+  //Check term checkbox is unchecked
+  await page.getByRole("checkbox").nth(1).click();
+  await expect(page.getByRole("checkbox").nth(1)).toHaveAttribute(
+    "data-state",
+    "unchecked"
+  );
+  await expect(page.locator("//span[text()='Place Order']")).toBeDisabled();
+  await page.waitForTimeout(1000);
+  await page.close();
+});
+test("Guest Please add card to process payment!", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  //Navigate to mainpage
+  await page.goto("/");
+  await expect(page).toHaveURL("/");
+  //Buy Voucher
+  await page.click("(//img[@class='object-cover'])[1]");
+  await page.getByRole("button", { name: "Buy Now" }).dblclick();
+  await page.waitForTimeout(3000);
+  //Add cart item
+  await expect(page).toHaveURL("/cart");
+  await page.getByRole("button", { name: "Proceed To Checkout" }).click();
+  await expect(page).toHaveURL(/.*checkout/);
+  //Add email
+  await page.click("input[placeholder='Enter email']");
+  await page.fill("input[placeholder='Enter email']", `${randomEmail}`);
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Place Order" }).dblclick();
+  await expect(
+    page.getByText("Please add card to process payment!")
+  ).toBeVisible();
+  page.close();
 });
