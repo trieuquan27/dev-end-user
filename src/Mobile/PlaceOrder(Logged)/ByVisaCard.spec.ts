7c9b312b1e219dad429d2ed06c9bbf88c152df1a
trieu_quan <trieu_quan@fastboy.net>
2023-07-14 11:31:42 +0700

    refactor mobile place order logged case

--- src/Mobile/PlaceOrder(Logged)/ByVisaCard.spec.ts
+++ src/Mobile/PlaceOrder(Logged)/ByVisaCard.spec.ts
@@ -1,6 +1,11 @@
 import { expect, test } from "@playwright/test";
 import { getApi } from "../../common/getapi";
-import { userName6, userName4, userName2 } from "../../common/AccountList";
+import {
+  userName6,
+  userName4,
+  userName2,
+  userName5,
+} from "../../common/AccountList";
 import {
   randomAddress,
   randomFirstName,
@@ -193,6 +198,40 @@ test("Mobile Place order by Master Card", async ({ page, context }) => {
   await page.getByRole("button", { name: "Add Card" }).click({ delay: 500 });
   await page.waitForTimeout(3000);
   await page.getByRole("button", { name: "Place Order" }).click({ delay: 500 });
+  await expect(page.getByText("Order placed successfully")).toBeVisible();
+  await expect(page).toHaveURL(/.*thank-you/);
+  await page.waitForTimeout(3000);
+  await page.click("//a[contains(text(),'View Order')]");
+  await expect(page).toHaveURL(/.*order/);
+  await expect(page.getByText("Receipt Details")).toBeVisible();
+  page.close();
+});
+
+test("Mobile Place order by GCI coin", async ({ page }) => {
+  await page.goto("/");
+  await page.click("//*[name()='path' and contains(@fill-rule,'evenodd')]");
+  await page.click("(//span[contains(@class,'flex items-center')])[3]");
+  await expect(page).toHaveURL("/signin");
+  // await page.fill('input[name="username"]', "a@a68.com");
+  await page.fill('input[name="username"]', `${userName5.Name}`);
+  await page.fill('input[name="password"]', `${userName5.Password}`);
+  await page.click("(//button[contains(@class,'flex items-center')])[1]");
+  await expect(page).toHaveURL("/");
+  await page
+    .getByRole("link", { name: "Price & Percentage Sort" })
+    .first()
+    .click();
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Buy Now" }).click({ delay: 300 });
+  await page.waitForTimeout(3000);
+  await expect(page).toHaveURL("/cart");
+  await expect(page.getByText("Price Details")).toBeVisible();
+  await page.getByRole("button", { name: "Proceed To Checkout" }).click();
+  await page.waitForTimeout(5000);
+  await expect(page).toHaveURL(/.*checkout/);
+  await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Place Order" }).click();
+  await page.waitForTimeout(3000);
   await expect(page.getByText("Order placed successfully")).toBeVisible();
   await expect(page).toHaveURL(/.*thank-you/);
   await page.waitForTimeout(3000);
